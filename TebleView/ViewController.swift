f2006b22bb5aabce1b9ae11deee0983cee5607db
CarlosRamírezGómez <CarlosDiplomado@macbook-2.local>
2019-03-08 19:52:45 -0600

    Initial Commit

---
+++ TebleView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TebleView
+//
+//  Created by CarlosRamírezGómez on 3/8/19.
+//  Copyright © 2019 CarlosRamírezGómez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
