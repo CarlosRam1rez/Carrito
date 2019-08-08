523ca0a07fd4bd5b8dce19156903c095dd8ac69a
Carlos Ramírez <carlosr2-d2@hotmail.com>
2019-08-07 22:48:00 -0500

    FirstCommit

---
+++ TebleView/CeldaTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CeldaTableViewCell.swift
+//  TebleView
+//
+//  Created by CarlosRamírezGómez on 3/8/19.
+//  Copyright © 2019 CarlosRamírezGómez. All rights reserved.
+//
+
+import UIKit
+
+class CeldaTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TebleView/DescriptionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DescriptionViewController.swift
+//  TebleView
+//
+//  Created by CarlosRamírezGómez on 3/9/19.
+//  Copyright © 2019 CarlosRamírezGómez. All rights reserved.
+//
+
+import UIKit
+
+class DescriptionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
